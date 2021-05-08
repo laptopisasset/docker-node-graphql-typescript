d6332ea3845096833b8c86565421ab4f26f31721
Soundhar <laptopisasset@gmail.com>
2021-05-08 12:27:40 +0530

    Added the database.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
 import app from "./app";
 
-app.listen(8080, () => console.log("Server listening on 8080"));
+import { connect } from "mongoose";
+
+connect(
+  `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+)
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Server listening on ${process.env.PORT}`)
+    );
+  })
+  .catch(console.error);
