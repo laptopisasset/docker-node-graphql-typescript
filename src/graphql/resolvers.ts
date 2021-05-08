b6267d993d9c19f080df8d4ab6d0a02d526fc124
Soundhar <laptopisasset@gmail.com>
2021-05-08 12:45:24 +0530

    Added validators.

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,8 +1,27 @@
 import User from "../models/user";
 import bcrypt from "bcryptjs";
+import validator from "validator";
 
 const createUser = async ({ userInput }: any, req: any) => {
   const existingUser = await User.findOne({ email: userInput.email });
+  const errors = [];
+
+  if (!validator.isEmail(userInput.email)) {
+    errors.push({ message: "E-mail is invalid" });
+  }
+
+  if (
+    validator.isEmpty(userInput.password) ||
+    !validator.isLength(userInput.password, { min: 5 })
+  ) {
+    errors.push({ message: "Password is too short!" });
+  }
+
+  if (errors.length > 0) {
+    const error = new Error("Invalid Input");
+    throw error;
+  }
+
   if (existingUser) {
     const error = new Error("User already exists");
     throw error;
