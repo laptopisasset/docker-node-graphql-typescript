6b2ca3619ec39aa619a5a692c05ae2574f424b6e
Soundhar <laptopisasset@gmail.com>
2021-05-08 13:39:46 +0530

    Added the mutation.

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,35 @@ import graphqlResolver from "./graphql/resolvers";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    customFormatErrorFn: (err) => {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = (err.originalError as any).data;
+      const message = err.message || `An error occured`;
+      const code = (err.originalError as any).code || 500;
+
+      return { message, status: code, data: data };
+    },
   })
 );
 
--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,7 @@
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import validator from "validator";
+import jwt from "jsonwebtoken";
 
 const createUser = async ({ userInput }: any, req: any) => {
   const existingUser = await User.findOne({ email: userInput.email });
@@ -18,7 +19,9 @@ const createUser = async ({ userInput }: any, req: any) => {
   }
 
   if (errors.length > 0) {
-    const error = new Error("Invalid Input");
+    const error: any = new Error("Invalid Input");
+    error.data = errors;
+    error.code = 400;
     throw error;
   }
 
@@ -35,6 +38,35 @@ const createUser = async ({ userInput }: any, req: any) => {
   return { ...createdUser._doc, _id: createdUser._id.toString() };
 };
 
+const login = async ({ email, password }: any, req: any) => {
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    const error: any = new Error("User not found.");
+    error.code = 401;
+    throw error;
+  }
+  const isEqual = await bcrypt.compare(password, user.password);
+  if (!isEqual) {
+    const error: any = new Error("Password is incorrect.");
+    error.code = 401;
+    throw error;
+  }
+  const token = jwt.sign(
+    {
+      userId: user._id.toString(),
+      email: user.email,
+    },
+    "somesupersecretsecret",
+    { expiresIn: "1h" }
+  );
+
+  return {
+    token,
+    userId: user._id.toString(),
+  };
+};
+
 export default {
   createUser,
+  login,
 };
