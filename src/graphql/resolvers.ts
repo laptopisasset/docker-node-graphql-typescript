df94315b71cfd5c4d15dbfdb01b4c08a2a1c840f
Soundhar <laptopisasset@gmail.com>
2021-05-08 12:16:10 +0530

    createUser resolver.

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,21 @@
-const User = require("../model/");
+import User from "../models/user";
+import bcrypt from "bcryptjs";
 
-const createUser = ({ userInput }: any, req: any) => {};
+const createUser = async ({ userInput }: any, req: any) => {
+  const existingUser = await User.findOne({ email: userInput.email });
+  if (existingUser) {
+    const error = new Error("User already exists");
+    throw error;
+  }
+  const hashedPw = await bcrypt.hash(userInput.password, 12);
+  const createdUser = await User.create({
+    email: userInput.email,
+    name: userInput.name,
+    password: hashedPw,
+  });
+  return { ...createdUser._doc, _id: createdUser._id.toString() };
+};
+
 export default {
   createUser,
 };
